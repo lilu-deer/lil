e0e8dc3380b1486c3b2f67035107c985322d54ed
xiaolu <xiaolu@DESKTOP-MKIIGTH>
2016-09-13 20:10:28 +0800

    add

---
+++ Demo3/src/com/text/text1.java
@@ -0,0 +1,14 @@
+package com.text;
+
+import java.util.Arrays;
+
+public class text1 {
+  public static void main(String[] args) {
+      System.out.println("begin"); 
+	  int[] arr={1,2,3,4,5,6,7,8,9,10};
+       
+      int index=Arrays.
+    		  binarySearch(arr, 4);
+     System.out.println(index);	 
+  }
+}
