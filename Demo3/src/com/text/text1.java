75ef842961aefe284c9485b638c8b6180542aa6e
xiaolu <xiaolu@DESKTOP-MKIIGTH>
2016-09-13 20:11:50 +0800

    update

--- Demo3/src/com/text/text1.java
+++ Demo3/src/com/text/text1.java
@@ -6,7 +6,7 @@ public class text1 {
   public static void main(String[] args) {
       System.out.println("begin"); 
 	  int[] arr={1,2,3,4,5,6,7,8,9,10};
-       
+      //dddddd
       int index=Arrays.
     		  binarySearch(arr, 4);
      System.out.println(index);	 
